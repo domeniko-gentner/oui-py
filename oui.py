d6589db06766f45dc71e5b64807b60c5a070f329
Domeniko Gentner <source@tuxstash.de>
2020-07-11 21:40:01 +0200

    Pep8, removed/merged some unnecessary code

--- oui.py
+++ oui.py
@@ -1,23 +1,23 @@
-from argparse import ArgumentParser
-from colorama import Fore, init
-from os.path import isfile, expanduser, isdir
-from os import stat, mkdir
-import requests
 import sys
-import re
+from argparse import ArgumentParser
+from os import stat, mkdir
+from os.path import isfile, expanduser, isdir
+from re import compile, match as rmatch
+from colorama import Fore, init
+from requests import get as r_get
 
 
 def download_oui_defs(fpath: str, force_dl=False) -> bool:
     # file exists and is not older than 1 week
     if (isfile(fpath) and stat(fpath).st_mtime > 604800) and not force_dl:
-        print(f"{Fore.CYAN}Definitions exists and is less than one week old, omitting download")
+        print(f"{Fore.CYAN}Definitions exist and file is less than one week old, omitting download")
         return True
     else:
         if force_dl:
             print(f"{Fore.LIGHTRED_EX}Download forced, please wait...")
         else:
             print(f"{Fore.CYAN}Definitions not found or too old, downloading file, please wait...")
-        r = requests.get("http://standards-oui.ieee.org/oui.txt")
+        r = r_get("http://standards-oui.ieee.org/oui.txt")
         if r.status_code == 200:
             with open(fpath, "wb") as fp:
                 fp.write(r.content)
@@ -29,9 +29,10 @@ def download_oui_defs(fpath: str, force_dl=False) -> bool:
 
 def parse_definitions(fpath: str) -> dict:
     result = dict()
+    pattern = compile("^[0-9A-F_]{2}[-][0-9A-F_]{2}[-][0-9A-F_]{2} .*$")
     with open(fpath, "rb") as fp_read:
         for line in fp_read:
-            match = re.match("^[0-9A-F_]{2}[-][0-9A-F_]{2}[-][0-9A-F_]{2} .*$", line.decode('utf8'))
+            match = rmatch(pattern, line.decode('utf8'))
             if match:
                 entry = match.group()
                 entry = entry.split("\t")
@@ -42,21 +43,16 @@ def parse_definitions(fpath: str) -> dict:
 
 
 def lookup(macs: dict, mac: str) -> bool:
-    mac = mac.upper()
-    if mac[0:8] in macs:
-        print(f"{Fore.GREEN}{mac} belongs to {macs[mac[0:8]]}")
+    vendor = mac[0:8].upper().replace(":", "-")
+    if vendor in macs:
+        print(f"{Fore.GREEN}{mac} belongs to {macs[vendor]}")
         return True
-    print(f"{Fore.RED}Couldn't find oui {mac[0:8]}")
+    print(f"{Fore.RED}Couldn't find oui {vendor}")
     return False
 
 
 if __name__ == "__main__":
     init(autoreset=True)
-    f_path = str()
-
-    if not isdir(expanduser("~/.oui")):
-        mkdir(expanduser("~/.oui"))
-    f_path = expanduser("~/.oui/oui.txt")
 
     parser = ArgumentParser(description="oui.py: MAC vendor lookup")
     parser.add_argument("mac", help="The MAC address to process")
@@ -66,19 +62,19 @@ if __name__ == "__main__":
 
     if args.file:
         f_path = args.file
+    else:
+        if not isdir(expanduser("~/.oui")):
+            mkdir(expanduser("~/.oui"))
+        f_path = expanduser("~/.oui/oui.txt")
 
     if not download_oui_defs(f_path, args.force):
         print(f"{Fore.RED}Something went wrong during the download")
         sys.exit(1)
 
-    mac_arg = args.mac
-    if mac_arg[2] == ":":
-        mac_arg = mac_arg.replace(":", "-")
-
-    if lookup(parse_definitions(f_path), mac_arg):
-        sys.exit(0)
-    else:
+    if not lookup(parse_definitions(f_path), args.mac):
         sys.exit(1)
+
+    sys.exit(1)
 
 
 
